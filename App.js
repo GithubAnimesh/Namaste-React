1fd6ddc93dc15ad933f01775efd6b4dfbf7b59f2
Animesh Singh <animesh.singh@gmail.com>
2024-08-31 11:32:08 +0530

    Episode - 04 &  Assignment-04

--- App.js
+++ App.js
@@ -1,19 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement('h1', {}, 'This is React form Akshay');
-// const jsxHeading = <h1 id="heading">This is JSX</h1>
-
-// react element inside react component.
-const Header2 = (
-     <h1 className="Header2">This is header2</h1>
-);
-
-const Header = () =>(
-    <div>
-    {Header2}
-     <h1 className="heading">This is React Funcational Component</h1>
+const Header = () => {
+     return (
+<div className="headers">
+     <div className="logo">
+          <img className="img-logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
      </div>
-);
+     <div className="nav-items">
+          <ul>
+               <li>Home</li>
+               <li>About Us</li>
+               <li>Contact Us</li>
+               <li>Cart</li>
+          </ul>
+     </div>
+</div>
+)
+};
+const resList = [
+     {
+         resName: "KFC Foods",
+         resCuisine: "Nonveg",
+         resRating: "4 stars",
+         resTime: "30 minutes"
+     },
+     {
+         resName: "Dominos",
+         resCuisine: "Pizza",
+         resRating: "4.4 stars",
+         resTime: "38 minutes"
+     },
+     {
+         resName: "MOMO",
+         resCuisine: "MOMO's",
+         resRating: "4.4 stars",
+         resTime: "38 minutes"
+     }
+ ];
+const RestaurantCard = (props) => {
+     const {resData} =  props;
+     return (
+          <div className="res-card">
+          <img alt="Card img" className="card-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/TopPicks/1KGOCMB" />
+          <h3>{resData.resName}</h3>
+          <h4>{resData.resCuisine}</h4>
+          <h4>{resData.resRating}</h4>
+          <h4>{resData.resTime}</h4>
+          </div>
+     )
+};
+const Body = () => {
+     return(
+          <div className="body">
+               <div className="search">Search</div>
+               <div className="res-container">
+               {
+                    resList.map((restaurant) => (
+                         <RestaurantCard key={restaurant.resId} resData={restaurant}/>
+                    ))
+               }
+               </div>
+          </div>
+     )
+};
+const AppBody = () => {
+return (
+     <div className="app">
+       <Header />
+       <Body />
+     </div>
+)
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Header/>);
+root.render(<AppBody/>);
